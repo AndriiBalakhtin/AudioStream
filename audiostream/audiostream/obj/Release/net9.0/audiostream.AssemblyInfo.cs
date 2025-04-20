2a4b1d97ee4fdf10118900f78676ac19ec5771ef
Aswel`a <andrii.balakhtin@ialedu.it>
2025-04-20 12:15:17 -0700

    sln to exe

--- audiostream/audiostream/obj/Release/net9.0/audiostream.AssemblyInfo.cs
+++ audiostream/audiostream/obj/Release/net9.0/audiostream.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("audiostream")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a70ccbc9ff6c967455ec6dcb63d5d089b89cb84")]
 [assembly: System.Reflection.AssemblyProductAttribute("audiostream")]
